ea92bef599e748eb3564a02496073be891bdbc71
ElenaGavrina <45360118+ElenaGavrina@users.noreply.github.com>
2023-04-18 22:50:25 +0300

    Update main.go

--- main.go
+++ main.go
@@ -16,10 +16,10 @@ func main (){
 
 func isPalindrom (some string) bool {
 	l :=strings.TrimSpace(strings.ToLower(some))
-    for i:=0; i < len(l)/2; i++ {
-    	if (l[i] != l[len(l) -1 - i])  {
+        for i:=0; i < len(l)/2; i++ {
+    	    if (l[i] != l[len(l) -1 - i])  {
     		return false
-    	}
+    	    }
   	}
   	return true
 }
